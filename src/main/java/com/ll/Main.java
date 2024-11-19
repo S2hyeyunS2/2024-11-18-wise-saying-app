93fe26cbe0143dc4f84991c0a41aa1b57c3ea70c
S2hyeyunS2 <hhyeyun22@gmail.com>
2024-11-19 14:32:37 +0900

    Scanner

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         App app = new App(); //new 다음에 나오는 것은  Class이다.
@@ -10,6 +12,14 @@ public class Main {
 class App{
     public void run(){
         System.out.println("==명언 앱 ==");
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.print("명령) ");
+        String cmd = scanner.nextLine();
+
+        System.out.println("입력된 명령어 : %s".formatted(cmd));
+
+        scanner.close();
     }
 }
