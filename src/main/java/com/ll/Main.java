17e7a391d9f27b0497ad32f36b1d31319c7e95bb
S2hyeyunS2 <hhyeyun22@gmail.com>
2024-11-20 10:17:22 +0900

    20일 시작

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,6 +1,6 @@
 package com.ll;
 
-public class main { // Applicaiton이 일하도록 함
+public class Main { // Applicaiton이 일하도록 함
     public static void main(String[] args) {
 
         Container.init(); // 공통적으로 사용되는 것들을 모아두는 공간(Container) 초기화
