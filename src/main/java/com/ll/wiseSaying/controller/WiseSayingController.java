4f4750b2ab7418c62022bed647f65ec1fe7f837a
S2hyeyunS2 <hhyeyun22@gmail.com>
2024-11-22 10:36:39 +0900

    ãservice 수정

--- src/main/java/com/ll/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/ll/wiseSaying/controller/WiseSayingController.java
@@ -2,9 +2,11 @@ package com.ll.wiseSaying.controller;
 
 import com.ll.Container;
 import com.ll.Rq;
+import com.ll.wiseSaying.entity.WiseSaying;
 import com.ll.wiseSaying.service.WiseSayingService;
 
 import java.io.IOException;
+import java.util.List;
 
 public class WiseSayingController {
 
@@ -20,27 +22,58 @@ public class WiseSayingController {
         System.out.print("작가 : ");
         String authorName = Container.getScanner().nextLine().trim();
 
-        wiseSayingService.create(content, authorName); // Service 호출
+        long id=wiseSayingService.create(content, authorName);
+        System.out.printf(id+"번 명언이 등록되었습니다.\n");
     }
 
     public void list() throws IOException { // 명언 목록 출력
-        wiseSayingService.list(); // Service 호출
+        List<WiseSaying> wiseSayings=wiseSayingService.list();
+
+        if(wiseSayings.isEmpty()){
+            System.out.println("등록된 명언이 없습니다.");
+            return;
+        }
+
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("-".repeat(30));
+
+        wiseSayings.stream()
+                .sorted((a, b) -> Long.compare(b.getId(), a.getId()))
+                .forEach(wiseSaying -> System.out.printf("%d / %s / %s\n",
+                        wiseSaying.getId(), wiseSaying.getAuthorName(), wiseSaying.getContent()));
     }
 
     public void remove(Rq rq) throws IOException { // 명언 삭제
         int id = rq.getIntParam("id", -1);
-        wiseSayingService.remove(id); // Service 호출
+
+        if (!wiseSayingService.remove(id)) { // 삭제 실패
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
+        } else { // 삭제 성공
+            System.out.printf("%d번 명언이 삭제되었습니다.\n", id);
+        }
     }
 
     public void modify(Rq rq) throws IOException { // 명언 수정
         int id = rq.getIntParam("id", -1);
+        if (id == -1) {
+            System.out.println("수정할 명언의 번호를 입력해주세요.");
+            return;
+        }
+
+        System.out.print("새 명언 : ");
         String newContent = Container.getScanner().nextLine().trim();
+        System.out.print("새 작가 : ");
         String newAuthorName = Container.getScanner().nextLine().trim();
 
-        wiseSayingService.modify(id, newContent, newAuthorName); // Service 호출
+        if (!wiseSayingService.modify(id, newContent, newAuthorName)) { // 수정 실패
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
+        } else { // 수정 성공
+            System.out.printf("%d번 명언이 수정되었습니다.\n", id);
+        }
     }
 
     public void build() throws IOException {
-        wiseSayingService.build(); // Service 호출
+        wiseSayingService.build(); // Service
+        System.out.println("data.json 파일의 내용이 갱신되었습니다.");// 호출
     }
 }
